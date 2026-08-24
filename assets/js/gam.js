8daf177f1a14d9660fd030e5efe99ef5adb22c8a
Adesh Sarwan <adeshsarwan@192.168.1.11>
2026-08-24 12:52:27 +0530

    Version 1  upload of folder

---
+++ assets/js/gam.js
@@ -0,0 +1,3 @@
+// GameWhame GAM integration placeholders. Replace paths after creating units in Google Ad Manager.
+window.GAMEWHAME_GAM={networkCode:"REPLACE_WITH_NETWORK_CODE",units:{homeTop:"/REPLACE/home_top",homeFeed:"/REPLACE/home_feed",gameTop:"/REPLACE/game_top",gameRail:"/REPLACE/game_rail",gameBelow:"/REPLACE/game_below",anchor:"/REPLACE/anchor",interstitial:"/REPLACE/web_interstitial",rewarded:"/REPLACE/web_rewarded"}};
+// GPT is intentionally not requested until real GAM paths are supplied.
